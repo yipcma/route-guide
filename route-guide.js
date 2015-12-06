bc5ea42f107fe895d1aa6d66e94e1f9e932cf798
Andrew Yip <yipcma@gmail.com>
2015-12-06 23:00:29 +0300

    using template-level subscriptions with Blaze, add check package

---
+++ route-guide.js
@@ -0,0 +1,37 @@
+Posts = new Mongo.Collection('posts');
+
+if (Meteor.isServer) {
+  Posts.remove({});
+  Posts.insert({
+    _id: "hello-world",
+    title: "Hello World Post",
+    content: "This is the content"
+  });
+  Meteor.publish('singlePost', function(id) {
+    check(id, String);
+    // Make a delay manually to show the loading state
+    Meteor._sleepForMs(1000);
+    return Posts.find({
+      _id: id
+    });
+  });
+}
+
+if (Meteor.isClient) {
+  Template.blogPost.onCreated(function() {
+    var self = this;
+    self.autorun(function() {
+      var postId = FlowRouter.getParam('postId');
+      self.subscribe('singlePost', postId);
+    });
+  });
+  Template.blogPost.helpers({
+    post: function() {
+      var postId = FlowRouter.getParam('postId');
+      var post = Posts.findOne({
+        _id: postId
+      }) || {};
+      return post;
+    }
+  });
+}
