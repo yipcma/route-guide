aec9650adde00530114df50cf1c7784077c0f4ee
Andrew Yip <yipcma@gmail.com>
2015-12-07 12:56:11 +0300

    using subsmanager, add reactive-var

--- route-guide.js
+++ route-guide.js
@@ -18,19 +18,28 @@ if (Meteor.isServer) {
 }
 
 if (Meteor.isClient) {
+  PostSubs = new SubsManager();
+
   Template.blogPost.onCreated(function() {
-    var self = this;
-    self.autorun(function() {
-      var postId = FlowRouter.getParam('postId');
-      self.subscribe('singlePost', postId);
-    });
+      var self = this;
+      self.ready = new ReactiveVar();
+      self.autorun(function() {
+          var postId = FlowRouter.getParam('postId');
+          var handle = PostSubs.subscribe('singlePost', postId);
+          self.ready.set(handle.ready());
+      });
   });
+
+  Template.blogPost.helpers({
+      postReady: function() {
+          return Template.instance().ready.get();
+      }
+  });
+
   Template.blogPost.helpers({
     post: function() {
       var postId = FlowRouter.getParam('postId');
-      var post = Posts.findOne({
-        _id: postId
-      }) || {};
+      var post = Posts.findOne({_id: postId}) || {};
       return post;
     }
   });
