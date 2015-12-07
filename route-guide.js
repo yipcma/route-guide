2db06c19289fb3fac9a78f11e89cd9b7e681949e
Andrew Yip <yipcma@gmail.com>
2015-12-07 13:32:24 +0300

    link generation, remove autopublish

--- route-guide.js
+++ route-guide.js
@@ -5,8 +5,14 @@ if (Meteor.isServer) {
   Posts.insert({
     _id: "hello-world",
     title: "Hello World Post",
+    category: "test",
     content: "This is the content"
   });
+
+  Meteor.publish('allPosts', function() {
+    return Posts.find();
+  });
+
   Meteor.publish('singlePost', function(id) {
     check(id, String);
     // Make a delay manually to show the loading state
@@ -18,28 +24,47 @@ if (Meteor.isServer) {
 }
 
 if (Meteor.isClient) {
+  Meteor.subscribe('allPosts');
   PostSubs = new SubsManager();
 
   Template.blogPost.onCreated(function() {
-      var self = this;
-      self.ready = new ReactiveVar();
-      self.autorun(function() {
-          var postId = FlowRouter.getParam('postId');
-          var handle = PostSubs.subscribe('singlePost', postId);
-          self.ready.set(handle.ready());
-      });
+    var self = this;
+    self.ready = new ReactiveVar();
+    self.autorun(function() {
+      var postId = FlowRouter.getParam('postId');
+      var handle = PostSubs.subscribe('singlePost', postId);
+      self.ready.set(handle.ready());
+    });
+  });
+
+  Template.blogHome.helpers({
+    posts: function() {
+      return Posts.find();
+    },
+    pathForPost: function() {
+      var post = this;
+      var params = {
+        category: post.category,
+        postId: post._id
+      };
+      var queryParams = {
+        comments: "yes"
+      };
+      var routeName = "blogPost";
+      var path = FlowRouter.path(routeName, params, queryParams);
+      return path;
+    }
   });
 
   Template.blogPost.helpers({
-      postReady: function() {
-          return Template.instance().ready.get();
-      }
-  });
-
-  Template.blogPost.helpers({
+    postReady: function() {
+      return Template.instance().ready.get();
+    },
     post: function() {
       var postId = FlowRouter.getParam('postId');
-      var post = Posts.findOne({_id: postId}) || {};
+      var post = Posts.findOne({
+        _id: postId
+      }) || {};
       return post;
     }
   });
